e529a5071cd96ae0cc7d8bd4338ea5162c7dc542
Wachirapol Klinkasorn <88503947+wachirapol2002@users.noreply.github.com>
2023-04-30 19:59:48 +0700

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: '',
+  password: 'password',
   database: 'webpro',
   waitForConnections: true,
   connectionLimit: 10,

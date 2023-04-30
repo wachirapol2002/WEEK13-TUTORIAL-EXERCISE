961a7747fbde2cd1f3272916495d84fb3480f616
Wachirapol Klinkasorn <88503947+wachirapol2002@users.noreply.github.com>
2023-04-30 20:05:40 +0700

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -7,7 +7,8 @@ const pool = mysql.createPool({
   database: 'webpro',
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
+  queueLimit: 0,
+  port: 3307
 });
 
 module.exports = pool;
